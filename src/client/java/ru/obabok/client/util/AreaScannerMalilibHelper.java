0cf1b666b97b1882269094835bd107b332431074
Alexresh <alexresh62@yandex.ru>
2026-08-01 11:36:07 +0300

    AreaScanner has been added to the Malilib registry

--- src/client/java/ru/obabok/client/util/AreaScannerMalilibHelper.java
+++ src/client/java/ru/obabok/client/util/AreaScannerMalilibHelper.java
@@ -1,9 +1,13 @@
 package ru.obabok.client.util;
 
 import fi.dy.masa.malilib.event.InitializationHandler;
+import fi.dy.masa.malilib.registry.Registry;
+import fi.dy.masa.malilib.util.data.ModInfo;
 import ru.obabok.client.AreaScannerClient;
 import ru.obabok.client.Config;
+import ru.obabok.client.gui.screens.ConfigGui;
 import ru.obabok.client.handlers.InitHandler;
+import ru.obabok.common.References;
 
 public class AreaScannerMalilibHelper {
     public static boolean shouldUpdateRealtime() {
@@ -15,5 +19,6 @@ public class AreaScannerMalilibHelper {
 
     public static void initMalilib(){
         InitializationHandler.getInstance().registerInitializationHandler(new InitHandler());
+        Registry.CONFIG_SCREEN.registerConfigScreenFactory(new ModInfo(References.MOD_ID, References.MOD_NAME, ConfigGui::new));
     }
 }
--- src/main/java/ru/obabok/common/References.java
+++ src/main/java/ru/obabok/common/References.java
@@ -5,5 +5,6 @@ import org.slf4j.LoggerFactory;
 
 public class References {
     public static final String MOD_ID = "areascanner";
+    public static final String MOD_NAME = "AreaScanner";
     public static Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 }
